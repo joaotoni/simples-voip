3c9f05ce2c2709c3c505e8e3b25c7cb5244a8b15
Rafael Sangali <rafaelsangali@outlook.com>
2022-07-06 15:42:58 -0300

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import { FooterStyled } from "./style";
+
+ export function Footer(){
+  return(
+    <FooterStyled>
+      <p>Todos os direitos reservados. Copyright ©2021 SIMPLE VOIP</p>
+    </FooterStyled>
+  )
+ }
