cd102d0a98686a4761fd20db550797a306d8fb7e
Joao Antonio <joaoantoniomartinst@gmail.com>
2022-07-16 09:49:35 -0300

    Arrumando o ultimo botão

--- src/components/Widget/index.tsx
+++ src/components/Widget/index.tsx
@@ -5,7 +5,7 @@ export function Widget() {
   return (
     <WhatsButton
       target={"_blank"}
-      href="https://api.whatsapp.com/send?phone=5511955505449&text=Temos%20interesse%20na%20plataforma%20Simples%20VOIP%20De%20Multi%20Atendimento"
+      href="https://wa.me/message/ECHLT45IPHXLF1"
     >
       <img width={"60px"} src={logoWhatsapp} alt="Whatsapp Logo" />
     </WhatsButton>
